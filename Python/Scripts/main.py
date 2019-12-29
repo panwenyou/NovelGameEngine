6c6938211bf1295422cee17c11aeff6502844339
panwenyou <panwenyo_2006@qq.com>
2019-12-29 20:22:39 +0800

    Add debug window

--- Python/Scripts/main.py
+++ Python/Scripts/main.py
@@ -7,14 +7,28 @@ from PyQt5.QtWidgets import QApplication, QWidget
 print sys.path
 
 from GUI.CentralWindow import CentralWindow
+from GUI.InputDlg import InputDlg
 
 from utils.model import Story
 
 
 __builtins__.cur_story = Story()
 
+
+def cb(data_dict):
+    code = data_dict['code']['widget'].text()
+    print "input:", code
+    exec(code)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = CentralWindow()
     __builtins__.cur_wnd = ex
+
+    debug_dlg = InputDlg()
+    debug_dlg.setGeometry(500, 100, 400, 100)
+    debug_dlg.buildInput((("code", u'代码'),), cb)
+    debug_dlg.show()
+
     sys.exit(app.exec_())
